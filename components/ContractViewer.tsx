913c647580188ccc07a950b5c77a9e7d815d22b6
pescaderomusic <garrett@pescaderomusic.com>
2026-05-07 20:23:54 -0600

    fix use client directive order

--- components/ContractViewer.tsx
+++ components/ContractViewer.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 'use client'
-import { useState } from 'react'
+import Link from 'next/link'import { useState } from 'react'
 import { useRouter } from 'next/navigation'
 import { createClient } from '@/lib/supabase/client'
 
